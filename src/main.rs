83c582570c368ccd4bf0ae95dc2562f98c7533da
Sunjay Varma <varma.sunjay@gmail.com>
2019-02-24 15:05:28 -0500

    Scaled up pixelated rendering

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use minifb::{self, Key, KeyRepeat};
 use tobj;
 use vek::*;
 
-struct Teapot<'a> {
+struct ToonShader<'a> {
     mvp: Mat4<f32>,
     positions: &'a [Vec3<f32>],
     normals: &'a [Vec3<f32>],
     light_dir: Vec3<f32>,
 }
 
-impl<'a> Pipeline for Teapot<'a> {
+impl<'a> Pipeline for ToonShader<'a> {
     type Vertex = u32; // Vertex index
     type VsOut = Vec3<f32>; // Normal
     type Pixel = u32; // BGRA
@@ -51,14 +51,51 @@ impl<'a> Pipeline for Teapot<'a> {
     }
 }
 
-const W: usize = 800;
-const H: usize = 600;
+fn scale_buffer<T: Clone + Copy>(target: &mut Buffer2d<T>, source: &Buffer2d<T>) {
+    let target_size = target.size();
+    let source_size = source.size();
+    let scale_x = target_size[0] / source_size[0];
+    let scale_y = target_size[1] / source_size[1];
+
+    // Check for truncating division
+    assert_eq!(source_size[0] * scale_x, target_size[0]);
+    assert_eq!(source_size[1] * scale_y, target_size[1]);
+
+    // Blit the pixels with no anti-aliasing
+    for i in 0..source_size[0] {
+        for j in 0..source_size[1] {
+            // Unsafe because we are guaranteeing that these indexes are not out of bounds
+            let color = unsafe { *source.get([i, j]) };
+
+            // Copy the color to every pixel in the scaled box
+            for sx in 0..scale_x {
+                for sy in 0..scale_y {
+                    // Unsafe because we are guaranteeing that these indexes are not out of bounds
+                    unsafe {
+                        target.set([i * scale_x + sx, j * scale_y + sy], color);
+                    }
+                }
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut color = Buffer2d::new([W, H], 0);
-    let mut depth = Buffer2d::new([W, H], 1.0);
+    let width = 32;
+    let height = 32;
+    let scale = 32;
 
-    let mut win = minifb::Window::new("Teapot", W, H, minifb::WindowOptions::default()).unwrap();
+    let mut color = Buffer2d::new([width, height], 0);
+    let mut depth = Buffer2d::new([width, height], 1.0);
+    // Scaled screen buffer
+    let mut screen = Buffer2d::new([width * scale, height * scale], 0);
+
+    let mut win = minifb::Window::new(
+        "Test Project",
+        width * scale,
+        height * scale,
+        minifb::WindowOptions::default()
+    ).unwrap();
 
     let obj = tobj::load_obj(&Path::new("samples/bigboi.obj")).unwrap();
     let indices = &obj.0[0].mesh.indices;
@@ -67,29 +104,26 @@ fn main() {
 
     for i in 0.. {
         let mvp =
-            Mat4::perspective_rh_no(1.3, 1.35, 0.01, 100.0) *
-            Mat4::<f32>::scaling_3d(0.2) *
-            Mat4::rotation_x((i as f32 * 0.002).sin() * 8.0) *
-            Mat4::rotation_y((i as f32 * 0.004).cos() * 4.0);
-            Mat4::rotation_z((i as f32 * 0.008).sin() * 2.0);
+            Mat4::perspective_rh_no(2.3, 1.00, 0.01, 100.0) *
+            Mat4::<f32>::scaling_3d(0.50) *
+            Mat4::rotation_x((i as f32 * 0.0004).sin() * 8.0) *
+            Mat4::rotation_y((i as f32 * 0.0008).cos() * 4.0);
+            Mat4::rotation_z((i as f32 * 0.0016).sin() * 2.0);
 
         color.clear(0);
         depth.clear(1.0);
 
-        Teapot {
+        ToonShader {
             mvp,
             positions: &positions,
             normals: &normals,
             light_dir: Vec3::new(1.0, 1.0, 1.0).normalized(),
-        }
-            .draw::<rasterizer::Triangles<_>, _>(
-                indices,
-                &mut color,
-                &mut depth,
-            );
+        }.draw::<rasterizer::Triangles<_>, _>(indices, &mut color, &mut depth);
 
-            win.update_with_buffer(color.as_ref()).unwrap();
+        scale_buffer(&mut screen, &color);
+
         if win.is_open() && !win.is_key_pressed(Key::Escape, KeyRepeat::No) {
+            win.update_with_buffer(screen.as_ref()).unwrap();
         } else {
             break;
         }
