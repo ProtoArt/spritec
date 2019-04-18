2e3b1e37e13012f3c0b0ceef6482b951e2dd3d03
Sunjay Varma <varma.sunjay@gmail.com>
2019-04-18 19:16:25 -0400

    Replaced several uses of unwrap with ?

--- src/loaders/obj.rs
+++ src/loaders/obj.rs
@@ -10,7 +10,7 @@ use crate::material::Material;
 use super::Model;
 
 pub fn load_file(path: impl AsRef<Path>) -> Result<Model, tobj::LoadError> {
-    let (meshes, materials) = tobj::load_obj(path.as_ref()).unwrap();
+    let (meshes, materials) = tobj::load_obj(path.as_ref())?;
     let materials: Vec<_> = materials
         .into_par_iter()
         .map(|mat| Arc::new(Material::from(mat)))
