8ceec9d49abfab5a5f3a866b6b6c9cf4ea48033d
Sunjay Varma <varma.sunjay@gmail.com>
2019-04-17 23:38:58 -0400

    A reasonable config format for spritec that allows us to express the types of tasks we currently support

--- src/config.rs
+++ src/config.rs
@@ -1,61 +1,126 @@
 use std::path::PathBuf;
-use std::collections::HashMap;
+use std::num::NonZeroUsize;
 
+use vek::Vec3;
 use serde::{Serialize, Deserialize};
 
-/// A configuration that represents the spritesheets that spritec should generate
+/// A configuration that represents the tasks that spritec should complete
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct SpriteConfig {
-    /// A mapping from output file path to the specification of the tiles of a spritesheet
-    spritesheets: HashMap<PathBuf, Spritesheet>,
+pub struct TaskConfig {
+    /// A list of the spritesheets for spritec to generate
+    spritesheets: Vec<Spritesheet>,
+    /// A list of individual poses for spritec to generate images for
+    poses: Vec<Pose>,
 }
+
+fn default_scale_factor() -> NonZeroUsize { NonZeroUsize::new(1).unwrap() }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Spritesheet {
-    /// The 2D layout of the frames on this spritesheet.
-    /// Each string can either be a filename or refer to something in `frames` (not both).
-    grid: Vec<SpritesheetRow>,
-    /// The specification of one or more frames. These can be referred to from within the grid in
-    /// order to specify additional properites of individual frames.
-    frames: Vec<FrameSpec>,
+    /// The path to output the generated spritesheet, relative to configuration file
+    path: PathBuf,
+    /// Animations to include in the spritesheet
+    animations: Vec<Animation>,
+    /// A scale factor to apply to the generated images. Each image is scaled without interpolation.
+    /// The value must be greater than zero. (default: 1).
+    #[serde(default = "default_scale_factor")]
+    scale: NonZeroUsize,
 }
 
-/// A single row of frames, represented by either explicitly writing out the frames or by
-/// generating their names with a repetition expression
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Animation {
+    frames: AnimationFrames,
+    /// The width at which to render each frame
+    frame_width: NonZeroUsize,
+    /// The height at which to render each frame
+    frame_height: NonZeroUsize,
+    /// The camera perspective from which to render each frame
+    camera: Camera,
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum SpritesheetRow {
-    Frames(Vec<String>),
-    Repetition(Repetition),
+pub enum AnimationFrames {
+    GltfFrames {
+        /// The path to a glTF file
+        gltf: PathBuf,
+        /// The name of the animation to select. Can be omitted if there is only a single animation
+        animation: Option<String>,
+        /// The frame to start the animation from (default: 0)
+        #[serde(default)]
+        start_frame: usize,
+        /// The frame to end the animation at (default: last frame of the animation)
+        end_frame: Option<usize>,
+    },
+    /// An array of filenames. OBJ files will be used as is. For glTF files, the model will be used
+    /// as loaded regardless of the animations present in the file.
+    Models(Vec<PathBuf>),
 }
 
-/// A specification of the properties of one or more frames.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct FrameSpec {
-    /// One or more filenames
-    file: FrameSpecFile,
+pub struct Pose {
+    model: PoseModel,
+    /// The width at which to render each frame
+    width: NonZeroUsize,
+    /// The height at which to render each frame
+    height: NonZeroUsize,
+    /// The camera perspective from which to render each frame
+    camera: Camera,
+    /// A scale factor to apply to the generated image. The image is scaled without interpolation.
+    /// The value must be greater than zero. (default: 1).
+    #[serde(default = "default_scale_factor")]
+    scale: NonZeroUsize,
 }
 
-/// A single filename, a list of filenames, or a repetition
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum FrameSpecFile {
-    Filename(PathBuf),
-    Filenames(Vec<PathBuf>),
-    Repetition(Repetition),
+pub enum PoseModel {
+    GltfFrame {
+        /// The path to a glTF file
+        gltf: PathBuf,
+        /// The name of the animation to select. Can be omitted if there is only a single animation
+        /// or if there is no animation.
+        animation: Option<String>,
+        /// The specific animation frame to render. The default is to render the first frame (or
+        /// the loaded pose of the model if there is no animation)
+        frame: Option<usize>,
+    },
+    /// A single filename. An OBJ file will be used as is. For a glTF file, the model will be
+    /// rendered as loaded regardless of the animations present in the file.
+    Model(PathBuf),
 }
 
-/// A repetitive pattern of a string. Expands into a list of strings where '{}' is replaced in the
-/// pattern with each number from start to end, inclusive.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Repetition {
-    pattern: String,
-    start: usize,
-    end: usize,
+#[serde(untagged)]
+pub enum Camera {
+    Perspective(Perspective),
+    Arbitrary {
+        /// The position of the camera (world coordinates)
+        /// Specify as an object: {x = 1.23, y = 4.56, z = 7.89}
+        position: Vec3<f32>,
+        /// The target position to look at (world coordinates)
+        /// Specify as an object: {x = 1.23, y = 4.56, z = 7.89}
+        target: Vec3<f32>,
+    },
 }
 
-impl From<Repetition> for Vec<String> {
-    fn from(rep: Repetition) -> Self {
-        (rep.start..=rep.end).map(|i| rep.pattern.replace("{}", &i.to_string())).collect()
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Perspective {
+    PerspectiveFront,
+    PerspectiveBack,
+    PerspectiveLeft,
+    PerspectiveRight,
+    PerspectiveTop,
+    PerspectiveBottom,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_bigboi_config() {
+        let conf_str = include_str!("../samples/bigboi/spritec.toml");
+        let _: TaskConfig = toml::from_str(conf_str).unwrap();
     }
 }
