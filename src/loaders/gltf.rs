b99e4f69b4989617f8be8b55aa06244e290456de
Sunjay Varma <varma.sunjay@gmail.com>
2019-04-18 01:05:33 -0400

    Ability to load files based on file extension + some extra parallelism where easy to do

--- src/loaders/gltf.rs
+++ src/loaders/gltf.rs
@@ -1,25 +1,24 @@
-use std::rc::Rc;
+use std::sync::Arc;
+use std::path::Path;
 
 use crate::geometry::Mesh;
 use crate::material::Material;
 
-pub fn load_file(filepath: &str) -> Vec<Mesh> {
-    let (document, buffers, _) = gltf::import(filepath).expect("Could not open Gltf file");
-
-    let mut ret: Vec<Mesh> = Vec::new();
+pub fn load_file(filepath: impl AsRef<Path>) -> Result<Vec<Mesh>, gltf::Error> {
+    let (document, buffers, _) = gltf::import(filepath)?;
 
     // Load all the materials first, this assumes that the material index
     // that primitive refers to is loaded in the same order as document.materials()
-    let materials: Vec<_> = document
-        .materials()
-        .map(|material| Rc::new(Material::from(material)))
+    let materials: Vec<_> = document.materials()
+        .map(|material| Arc::new(Material::from(material)))
         .collect();
 
+    let mut meshes = Vec::new();
     for mesh in document.meshes() {
         for primitive in mesh.primitives() {
-            ret.push(Mesh::from_gltf(&buffers, &primitive, &materials));
+            meshes.push(Mesh::from_gltf(&buffers, &primitive, &materials));
         }
     }
 
-    ret
+    Ok(meshes)
 }
