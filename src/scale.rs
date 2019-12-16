66248618a4fdbbb80b01909b85d8c72daa4e1a2e
Sunjay Varma <varma.sunjay@gmail.com>
2019-12-16 16:06:28 -0500

    Fixing bugs in scale implementation

--- src/scale.rs
+++ src/scale.rs
@@ -13,9 +13,19 @@ pub fn truncate_centered(source: &RgbaImage, target: &mut RgbaImage) {
     assert!(target_width <= source_width);
     assert!(target_height <= source_height);
 
+    // The offset within the source to copy pixels from
     let offset_x = source_width / 2 - target_width / 2;
     let offset_y = source_height / 2 - target_height / 2;
-    copy(source, target, (offset_x, offset_y));
+
+    for x in 0..target_width {
+        for y in 0..target_height {
+            let source_x = x + offset_x;
+            let source_y = y + offset_y;
+            let pixel = *source.get_pixel(source_x, source_y);
+
+            target.put_pixel(x, y, pixel);
+        }
+    }
 }
 
 /// Scales the given source image to fit into the target image.
@@ -58,6 +68,9 @@ pub fn scale(source: &RgbaImage, target: &mut RgbaImage) {
 pub fn copy(source: &RgbaImage, target: &mut RgbaImage, (offset_x, offset_y): (u32, u32)) {
     let source_width = source.width();
     let source_height = source.height();
+
+    assert!(offset_x + source_width <= target.width());
+    assert!(offset_y + source_height <= target.height());
 
     for x in 0..source_width {
         for y in 0..source_height {
