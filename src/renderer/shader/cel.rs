9f6c3ae372501726cefcfae2b8149455ec36e6a1
Sunjay Varma <varma.sunjay@gmail.com>
2020-01-17 20:30:18 -0500

    Base implementation of point, spot, and directional light support in cel shader

--- src/renderer/shader/cel.rs
+++ src/renderer/shader/cel.rs
@@ -2,16 +2,18 @@ use crate::math::Mat4;
 use glium::uniforms::{Uniforms, UniformValue};
 
 use crate::scene::Material;
-use crate::renderer::DirectionalLight;
+use crate::renderer::Light;
 
 use super::nested_uniforms::NestedUniforms;
-pub use super::light_uniform::DirectionalLightUniform;
+pub use super::light_uniform::LightUniform;
 pub use super::material_uniform::MaterialUniform;
 
 pub struct CelUniforms<'a> {
     pub mvp: Mat4,
-    pub model_view_inverse_transpose: Mat4,
-    pub light: &'a DirectionalLight,
+    pub model_transform: Mat4,
+    pub model_inverse_transpose: Mat4,
+    pub light: &'a Light,
+    pub light_world_transform: Mat4,
     pub ambient_intensity: f32,
     pub material: &'a Material,
 }
@@ -19,18 +21,27 @@ pub struct CelUniforms<'a> {
 /// This struct must match the uniforms in the cel shaders
 pub struct Cel {
     mvp: UniformValue<'static>,
-    model_view_inverse_transpose: UniformValue<'static>,
-    light: DirectionalLightUniform,
+    model_transform: UniformValue<'static>,
+    model_inverse_transpose: UniformValue<'static>,
+    light: LightUniform,
     ambient_intensity: UniformValue<'static>,
     material: MaterialUniform,
 }
 
 impl Uniforms for Cel {
     fn visit_values<'a, F: FnMut(&str, UniformValue<'a>)>(&'a self, mut visit: F) {
-        let Self {mvp, model_view_inverse_transpose, light, ambient_intensity, material} = self;
+        let Self {
+            mvp,
+            model_transform,
+            model_inverse_transpose,
+            light,
+            ambient_intensity,
+            material,
+        } = self;
 
         visit("mvp", *mvp);
-        visit("model_view_inverse_transpose", *model_view_inverse_transpose);
+        visit("model_transform", *model_transform);
+        visit("model_inverse_transpose", *model_inverse_transpose);
         light.visit_nested("light", &mut visit);
         visit("ambient_intensity", *ambient_intensity);
         material.visit_nested("material", &mut visit);
@@ -39,12 +50,21 @@ impl Uniforms for Cel {
 
 impl<'a> From<CelUniforms<'a>> for Cel {
     fn from(cel_uniforms: CelUniforms<'a>) -> Self {
-        let CelUniforms {mvp, model_view_inverse_transpose, light, ambient_intensity, material} = cel_uniforms;
+        let CelUniforms {
+            mvp,
+            model_transform,
+            model_inverse_transpose,
+            light,
+            light_world_transform,
+            ambient_intensity,
+            material,
+        } = cel_uniforms;
 
         Self {
             mvp: UniformValue::Mat4(mvp.into_col_arrays()),
-            model_view_inverse_transpose: UniformValue::Mat4(model_view_inverse_transpose.into_col_arrays()),
-            light: DirectionalLightUniform::new(light),
+            model_transform: UniformValue::Mat4(model_transform.into_col_arrays()),
+            model_inverse_transpose: UniformValue::Mat4(model_inverse_transpose.into_col_arrays()),
+            light: LightUniform::new(light, light_world_transform),
             ambient_intensity: UniformValue::Float(ambient_intensity),
             material: MaterialUniform::new(material),
         }
--- src/renderer/shader/cel.vs
+++ src/renderer/shader/cel.vs
@@ -2,18 +2,25 @@
 
 // The Model View Projection matrix
 uniform mat4 mvp;
-// The transpose of the inverse of the matrix view * model, used for
+// The model matrix
+uniform mat4 model_transform;
+// The transpose of the inverse of the model matrix, used for
 // transforming the vertex's normal
-uniform mat4 model_view_inverse_transpose;
+uniform mat4 model_inverse_transpose;
 
 in vec3 position;
 in vec3 normal;
 
+// The normal, in the world coordinate system
 out vec3 v_normal;
+// The position, in the world coordinate system
+out vec3 v_position;
 
 void main() {
     // Transform normals to preserve orthogonality after non-uniform transformations.
-    v_normal = mat3(model_view_inverse_transpose) * normal;
+    v_normal = mat3(model_inverse_transpose) * normal;
+    v_position = vec3(model_transform * vec4(position, 1.0));
+
     // Transforms the position to screen space
     gl_Position = mvp * vec4(position, 1.0);
 }
