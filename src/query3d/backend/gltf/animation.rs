ffd40d7cd458a126ea0f91bdb4f69ed5cb5bc5db
Sunjay Varma <varma.sunjay@gmail.com>
2020-02-17 16:45:19 -0500

    Concise from_animation function by moving code into structs/impls

--- src/query3d/backend/gltf/animation.rs
+++ src/query3d/backend/gltf/animation.rs
@@ -4,24 +4,84 @@ use crate::query3d::query::AnimationPosition;
 use crate::math::{Milliseconds, Vec3, Quaternion, Mat4, Mat3, Decompose};
 use crate::scene::NodeId;
 
-use super::keyframes::{Keyframes, Frame};
+use super::keyframes::Keyframes;
 use super::interpolate::Interpolation;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
+pub struct AnimationSet {
+    anims: Vec<Animation>,
+}
+
+impl AnimationSet {
+    pub fn animation_mut(&mut self, name: Option<&str>) -> Option<&mut Animation> {
+        // By construction, there should only ever be one animation with a given name
+        self.anims.iter_mut().find(|anim| anim.name.as_deref() == name)
+    }
+
+    /// Inserts an animation into the animation set. You guarantee that no previously inserted
+    /// animation has the same name.
+    pub fn insert(&mut self, anim: Animation) -> &mut Animation {
+        self.anims.push(anim);
+        // This unwrap is safe because we just pushed in an animation
+        self.anims.last_mut().unwrap()
+    }
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct Animation {
     pub name: Option<String>,
-    pub scale_keyframes: Option<Keyframes<Vec3>>,
-    pub rotation_keyframes: Option<Keyframes<Quaternion>>,
-    pub translation_keyframes: Option<Keyframes<Vec3>>,
+    pub scale: Option<Keyframes<Vec3>>,
+    pub rotation: Option<Keyframes<Quaternion>>,
+    pub translation: Option<Keyframes<Vec3>>,
 }
 
 impl Animation {
     // with_name takes Option instead of String to include with Animations without names
-    pub fn with_name(name: Option<String>) -> Self {
+    pub fn with_name(name: Option<&str>) -> Self {
         Self {
-            name,
+            name: name.map(String::from),
             ..Self::default()
         }
+    }
+
+    pub fn add_keyframes(
+        &mut self,
+        channel: gltf::animation::Channel,
+        buffers: &[gltf::buffer::Data],
+        interpolation: Interpolation,
+    ) {
+        let reader = channel.reader(|buffer| Some(&buffers[buffer.index()]));
+        let times = reader.read_inputs().expect("Animation detected with no sampler input values")
+            .map(|time| Milliseconds::from_sec(time));
+
+        use gltf::animation::util::ReadOutputs::*;
+        match reader.read_outputs().expect("Animation detected with no sampler output values") {
+            Scales(scales) => {
+                assert!(self.scale.is_none(),
+                    "Did not expect animation with the same name to have multiple sets of scale keyframes");
+
+                let values = scales.map(Vec3::from);
+                self.scale = Some(Keyframes::new(times, values, interpolation));
+            },
+
+            Rotations(rotations) => {
+                assert!(self.rotation.is_none(),
+                    "Did not expect animation with the same name to have multiple sets of rotation keyframes");
+
+                let values = rotations.into_f32().map(|[x, y, z, w]| Quaternion::from_xyzw(x, y, z, w));
+                self.rotation = Some(Keyframes::new(times, values, interpolation));
+            },
+
+            Translations(translations) => {
+                assert!(self.translation.is_none(),
+                    "Did not expect animation with the same name to have multiple sets of translation keyframes");
+
+                let values = translations.map(Vec3::from);
+                self.translation = Some(Keyframes::new(times, values, interpolation));
+            },
+
+            MorphTargetWeights(_) => unimplemented!("Morph target animations are not supported yet"),
+        };
     }
 
     /// Application of animation data by decomposing the current node's transformation matrix and
@@ -29,15 +89,15 @@ impl Animation {
     pub fn apply_at(&self, transform_matrix: &Mat4, pos: &AnimationPosition) -> Mat4 {
         let mut matrix_transforms = transform_matrix.decompose();
 
-        if let Some(keyframes) = &self.scale_keyframes {
+        if let Some(keyframes) = &self.scale {
             let new_value = keyframes.value_at(pos);
             matrix_transforms.scale = new_value;
         }
-        if let Some(keyframes) = &self.rotation_keyframes {
+        if let Some(keyframes) = &self.rotation {
             let new_value = keyframes.value_at(pos);
             matrix_transforms.rotation = Mat3::from(new_value);
         }
-        if let Some(keyframes) = &self.translation_keyframes {
+        if let Some(keyframes) = &self.translation {
             let new_value = keyframes.value_at(pos);
             matrix_transforms.translation = new_value;
         }
@@ -49,66 +109,25 @@ impl Animation {
 pub fn from_animations<'a>(
     doc_anims: impl Iterator<Item=gltf::Animation<'a>>,
     buffers: &[gltf::buffer::Data],
-) -> HashMap<NodeId, Vec<Animation>> {
-    let mut animations = HashMap::new();
+) -> HashMap<NodeId, AnimationSet> {
+    let mut animations: HashMap<NodeId, AnimationSet> = HashMap::new();
 
     for anim_data in doc_anims {
         let anim_name = anim_data.name();
+
         for channel in anim_data.channels() {
-            let reader = channel.reader(|buffer| Some(&buffers[buffer.index()]));
-            let interpolation = match channel.sampler().interpolation() {
-                gltf::animation::Interpolation::Linear => Interpolation::Linear,
-                gltf::animation::Interpolation::Step => Interpolation::Step,
-                //TODO - In order to support cubicspline interpolation, we need to change how we're storing the data
-                // https://github.com/KhronosGroup/glTF/tree/master/specification/2.0#animation-samplerinterpolation
-                gltf::animation::Interpolation::CubicSpline => unimplemented!("Cubicspline interpolation is not supported!"),
-            };
+            let interpolation = channel.sampler().interpolation().into();
 
             // Create Animation
-            let anims = animations.entry(NodeId::from_gltf(&channel.target().node())).or_insert_with(|| Vec::new());
-            let anim = anims.iter_mut().find(|a: &&mut Animation| a.name.as_deref() == anim_name);
-            let mut anim = match anim {
+            let node_id = NodeId::from_gltf(&channel.target().node());
+            let anim_set = animations.entry(node_id).or_default();
+
+            let mut anim = match anim_set.animation_mut(anim_name) {
                 Some(anim) => anim,
-                None => {
-                    anims.push(Animation::with_name(anim_name.map(String::from)));
-                    // This unwrap is safe because we just pushed in an animation
-                    anims.last_mut().unwrap()
-                }
+                None => anim_set.insert(Animation::with_name(anim_name)),
             };
 
-            // Create Keyframes
-            use gltf::animation::util::ReadOutputs::*;
-            let key_times = reader.read_inputs().expect("Animation detected with no sampler input values");
-            match reader.read_outputs().expect("Animation detected with no sampler output values") {
-                Scales(scale) => {
-                    assert!(anim.scale_keyframes.is_none(), "Did not expect animation with the same name to have multiple sets of scale keyframes");
-                    let keyframes = Keyframes {
-                        frames: key_times.zip(scale)
-                        .map(|(time, output)| Frame {time: Milliseconds::from_sec(time), value: Vec3::from(output)}).collect::<Vec<Frame<Vec3>>>(),
-                        interpolation,
-                    };
-                    anim.scale_keyframes = Some(keyframes);
-                },
-                Rotations(rot) => {
-                    assert!(anim.rotation_keyframes.is_none(), "Did not expect animation with the same name to have multiple sets of rotation keyframes");
-                    let keyframes = Keyframes {
-                        frames: key_times.zip(rot.into_f32())
-                        .map(|(time, [x, y, z, w])| Frame {time: Milliseconds::from_sec(time), value: Quaternion::from_xyzw(x, y, z, w)}).collect::<Vec<Frame<Quaternion>>>(),
-                        interpolation,
-                    };
-                    anim.rotation_keyframes = Some(keyframes);
-                },
-                Translations(trans) => {
-                    assert!(anim.translation_keyframes.is_none(), "Did not expect animation with the same name to have multiple sets of translation keyframes");
-                    let keyframes = Keyframes {
-                        frames: key_times.zip(trans)
-                        .map(|(time, output)| Frame {time: Milliseconds::from_sec(time), value: Vec3::from(output)}).collect::<Vec<Frame<Vec3>>>(),
-                        interpolation,
-                    };
-                    anim.translation_keyframes = Some(keyframes);
-                },
-                MorphTargetWeights(_) => unimplemented!("Morph target animations are not supported yet"),
-            };
+            anim.add_keyframes(channel, buffers, interpolation);
         }
     }
 
