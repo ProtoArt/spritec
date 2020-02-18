d7b5f3ba628047afa1fb509c330966af461f6827
daose <15278132+daose@users.noreply.github.com>
2020-02-17 22:44:52 -0500

    gui: determine smart names for cameras

--- spritec_gui/components/Import/actions.js
+++ spritec_gui/components/Import/actions.js
@@ -20,9 +20,12 @@ const loadGLTF = path => dispatch => {
       scene = gltf.scene;
 
       // load the camera info first before loading the model
-      const cameras = gltf.cameras.map(camera => camera.name);
+      const cameras = gltf.cameras.map(camera => ({
+        id: camera.id,
+        name: parseCameraName(camera),
+      }));
       if (cameras.length > 0) {
-        dispatch(loadCamera(cameras[0]));
+        dispatch(loadCamera(cameras[0].id));
       }
 
       dispatch(actions.loadModel({path, cameras}));
@@ -32,15 +35,15 @@ const loadGLTF = path => dispatch => {
   );
 };
 
-const loadCamera = cameraName => dispatch => {
-  let cameraObj = scene.getObjectByName(cameraName);
+const loadCamera = id => dispatch => {
+  let cameraObj = scene.getObjectById(id);
   // Render the scene with the camera so that the camera properties are
   // updated with respect to the world.
   renderer.render(scene, cameraObj);
   cameraObj.matrixWorld.decompose(position, rotation, scale);
 
   dispatch(actions.setCamera({
-    name: cameraName,
+    id,
     position: position.toArray(),
     rotation: rotation.toArray(),
     scale: scale.toArray(),
@@ -51,6 +54,31 @@ const loadCamera = cameraName => dispatch => {
     fov: cameraObj.fov,
   }));
 };
+
+/*
+ * Parses the camera name from a camera object. The name depends on what meta information
+ * is available. The precedence is:
+ *
+ * 1. cameraName (parentName)
+ * 2. cameraName
+ * 3. parentName
+ * 4. threeID
+ */
+const parseCameraName = cameraObj => {
+  if (cameraObj.name && cameraObj.parent && cameraObj.parent.name) {
+    return `${cameraObj.name} (${cameraObj.parent.name})`;
+  }
+
+  if (cameraObj.name) {
+    return cameraObj.name;
+  }
+
+  if (cameraObj.parent && cameraObj.parent.name) {
+    return cameraObj.parent.name;
+  }
+
+  return `Camera_${cameraObj.id}`;
+}
 
 module.exports = {
   loadGLTF,
