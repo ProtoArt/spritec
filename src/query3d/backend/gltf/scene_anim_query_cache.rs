7ffe3d64746f5f2492b2f9e5e0283c678e3f9f23
Sunjay Varma <varma.sunjay@gmail.com>
2020-02-18 01:15:31 -0500

    Finished implementing animation position based cache

--- src/query3d/backend/gltf/scene_anim_query_cache.rs
+++ src/query3d/backend/gltf/scene_anim_query_cache.rs
@@ -1,6 +1,50 @@
+use std::cmp::Ordering;
 use std::collections::HashMap;
 
+use approx::relative_eq;
+
 use crate::query3d::{AnimationQuery, AnimationPosition};
+
+/// If this value is too small, our cache will be bloated. If the value is too big, the cache will
+/// incorrectly treat different values as the same.
+const TOLERANCE: f32 = 0.0001;
+
+/// Defines a total ordering for floating point numbers by treating numbers that are approximately
+/// the same as equal.
+///
+/// This does not account for NaN values and will probably panic if they are encountered.
+fn approx_cmp(left: f32, right: f32) -> Ordering {
+    if relative_eq!(left, right, epsilon = TOLERANCE) {
+        Ordering::Equal
+    } else {
+        // This will panic if we get NaN
+        left.partial_cmp(&right).unwrap()
+    }
+}
+
+/// Defines a total ordering for animation positions
+fn pos_cmp(left: &AnimationPosition, right: &AnimationPosition) -> Ordering {
+    use AnimationPosition::*;
+    match (left, right) {
+        // Arbitrarily order the different variants
+        (Time(_), RelativeTime {..}) => Ordering::Less,
+        (RelativeTime {..}, Time(_)) => Ordering::Greater,
+
+        // Compare the values if the variants are the same
+        (Time(left), Time(right)) => approx_cmp(left.to_msec(), right.to_msec()),
+
+        (
+            &RelativeTime {start_time: left_start_time, weight: left_weight},
+            &RelativeTime {start_time: right_start_time, weight: right_weight},
+        ) => {
+            // Arbitrarily decided to order by start_time first and then weight
+            match approx_cmp(left_start_time.to_msec(), right_start_time.to_msec()) {
+                Ordering::Equal => approx_cmp(left_weight, right_weight),
+                order => order,
+            }
+        },
+    }
+}
 
 /// A cache for animation positions
 ///
@@ -23,11 +67,18 @@ impl<T> Default for AnimPosCache<T> {
 
 impl<T> AnimPosCache<T> {
     pub fn get(&self, pos: &AnimationPosition) -> Option<&T> {
-        todo!()
+        self.find(pos).ok().map(|i| &self.entries[i].1)
     }
 
     pub fn insert(&mut self, pos: &AnimationPosition, value: T) {
-        todo!()
+        match self.find(pos) {
+            Ok(_) => unreachable!("bug: attempt to re-insert a cached value"),
+            Err(i) => self.entries.insert(i, (pos.clone(), value)),
+        }
+    }
+
+    fn find(&self, pos: &AnimationPosition) -> Result<usize, usize> {
+        self.entries.binary_search_by(|(epos, _)| pos_cmp(epos, pos))
     }
 }
 
