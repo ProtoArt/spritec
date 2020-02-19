6710a740529d89d8fe9930fff574f9b9a45047c1
daose <15278132+daose@users.noreply.github.com>
2020-02-19 11:11:38 -0500

    GUI: add input to adjust number of steps

--- spritec_gui/components/Import/ImportPanel.js
+++ spritec_gui/components/Import/ImportPanel.js
@@ -1,4 +1,4 @@
-const { actions } = require('./slice');
+const {actions} = require('./slice');
 const Component = require('../../lib/Component');
 
 class ImportPanel extends Component {
@@ -6,6 +6,7 @@ class ImportPanel extends Component {
     this.state = {
       inputWidth: element.querySelector('#input-width'),
       inputHeight: element.querySelector('#input-height'),
+      inputSteps: element.querySelector('#input-steps'),
     }
     this.state.inputWidth.onchange = (event) => {
       this.dispatch(actions.setWidth(Number(event.target.value)));
@@ -13,18 +14,25 @@ class ImportPanel extends Component {
     this.state.inputHeight.onchange = (event) => {
       this.dispatch(actions.setHeight(Number(event.target.value)));
     }
+    this.state.inputSteps.oninput = (event) => {
+      this.dispatch(actions.setAnimationSteps(Number(event.target.value)));
+    }
   }
 
   mapStateToProps() {
     return {
       width: (state) => state.import.selected.width,
       height: (state) => state.import.selected.height,
+      steps: (state) => state.import.selected.animation_total_steps,
+      animation: (state) => state.import.selected.animation,
     };
   }
 
   render() {
     this.state.inputWidth.value = this.props.width;
     this.state.inputHeight.value = this.props.height;
+    this.state.inputSteps.value = this.props.steps;
+    this.state.inputSteps.disabled = (this.props.animation === null);
   }
 }
 
