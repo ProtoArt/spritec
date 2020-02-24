feb0d9127bab7afd2ad327511b66e61fc610ccbe
daose <15278132+daose@users.noreply.github.com>
2020-02-23 23:45:57 -0500

    add export functionality

---
+++ spritec_gui/components/Export/ExportModal.js
@@ -0,0 +1,44 @@
+const {basename, extname} = require('path');
+const Component = require('../../lib/Component');
+const {actions} = require('../Import/slice');
+const {exportSprites} = require('./actions');
+
+class ExportModal extends Component {
+  onCreate(element) {
+    this.state = {
+      scaleText: element.querySelector('#export-scale-text'),
+      scaleElement: element.querySelector('#export-scale'),
+      submitButton: element.querySelector('#export-modal-button'),
+    };
+
+    element.querySelector('#export-form').onsubmit = event => {
+      // Use onsubmit for form validation but don't actually submit
+      event.preventDefault();
+      this.dispatch(exportSprites(event.target));
+    };
+
+    this.state.scaleElement.oninput = event => {
+      this.dispatch(actions.setScale(Number(event.target.value)));
+    };
+  }
+
+  mapStateToProps() {
+    return {
+      scale: state => state.import.selected.scale,
+      submitting: state => state.import.submitting,
+      width: state => state.import.selected.width,
+      height: state => state.import.selected.height,
+    };
+  }
+
+  render() {
+    const {width, height, scale, submitting} = this.props;
+    this.state.submitButton.disabled = submitting;
+    this.state.submitButton.value = submitting ? 'Exporting' : 'Export';
+    this.state.scaleText.innerText =
+      `${scale}x (${width * scale}x${height * scale})`;
+    this.state.scaleElement.value = scale;
+  }
+}
+
+module.exports = ExportModal;
--- spritec_gui/main.js
+++ spritec_gui/main.js
@@ -3,21 +3,23 @@ const CameraList = require('./components/Import/CameraList');
 const ImportCanvas = require('./components/Import/ImportCanvas');
 const ImportPanel = require('./components/Import/ImportPanel');
 const importSlice = require('./components/Import/slice');
+const ExportModal = require('./components/Export/ExportModal');
 const ModelList = require('./components/Import/ModelList');
 const {configureStore} = require('@reduxjs/toolkit');
 
 const store = configureStore({
   reducer: {
-    import: importSlice.reducer
+    import: importSlice.reducer,
   },
 });
 
 let components = [
-  new ImportCanvas(document.querySelector('#spritec-container'), store),
-  new ImportPanel(document.querySelector('#spritec-import'), store),
-  new ModelList(document.querySelector('#spritec-model-list'), store),
-  new CameraList(document.querySelector('#spritec-camera-list'), store),
-  new AnimationList(document.querySelector('#spritec-animation-list'), store),
+  new ImportCanvas(document.getElementById('spritec-container'), store),
+  new ImportPanel(document.getElementById('import-panel'), store),
+  new ModelList(document.getElementById('spritec-model-list'), store),
+  new CameraList(document.getElementById('spritec-camera-list'), store),
+  new AnimationList(document.getElementById('spritec-animation-list'), store),
+  new ExportModal(document, store),
 ];
 
 store.subscribe(() => {
